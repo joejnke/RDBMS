5c83c5a17d2c5578f3a4095e2c266d452182be14
Kirubel K <joejnke@gmail.com>
2020-02-04 23:36:04 +0300

    Recursive tuples

--- rdbms/util/tuples.hpp
+++ rdbms/util/tuples.hpp
@@ -1,24 +1,17 @@
+#include<boost/variant.hpp>
 #include<iostream>
 #include<set>
 #include<string>
-#include<boost/variant.hpp>
 
 using namespace std;
 
 template<class T>
 class tuples {
+    private:
+        set<set<T>> elements;
     public:
-        set<T> tuple1(T a);
-
-        set<set<T>> tuple2(T a,T b);
-
-        set<set<set<T>>> tuple3(T a,T b,T c);
-
-        set<set<set<set<T>>>> tuple4(T a,T b,T c,T d);
-
-        set<set<set<set<set<T>>>>> tuple5(T a,T b,T c,T d,T e);
-
-        set<set<set<set<set<set<T>>>>>> tuple6(T a,T b,T c,T d,T e,T f);
-
-        set<set<set<set<set<set<set<T>>>>>>> tuple7(T a,T b,T c,T d,T e,T f,T g);
+        tuples();
+        tuples(T elm1, T... elms);
+        // get type of every element
+        // toString the tuple
 };
