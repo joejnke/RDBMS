7c5f4b7580a1744a791800fd1b9dd27f274d1d33
= <=>
2020-02-05 16:34:19 +0300

    Added function arg for operations class

--- rdbms/types/operations.cpp
+++ rdbms/types/operations.cpp
@@ -6,26 +6,26 @@
 #include <iostream>
 #include <set>
 
-std::set<tuples> operations::union(){
+std::set<tuples> operations::union(std::set<tuples> R1 ,std::set<tuples> R2){
     //TODO
 }
 
-std::set<tuples> operations::intersection(){
+std::set<tuples> operations::intersection(std::set<tuples> R1 ,std::set<tuples> R2){
     //TODO
 }
 
-std::set<tuples> operations::difference(){
+std::set<tuples> operations::difference(std::set<tuples> R1 ,std::set<tuples> R2){
     //TODO
 }
 
-std::set<tuples> operations::projection(){
+std::set<tuples> operations::projection(std::set<tuples> R1){
     //TODO
 }
 
-std::set<tuples> operations::selection(){
+std::set<tuples> operations::selection(std::set<tuples> R1){
     //TODO
 }
 
-std::set<tuples> operations::natural_join(){
+std::set<tuples> operations::natural_join(std::set<tuples> R1 ,std::set<tuples> R2){
     //TODO
 }
--- rdbms/types/operations.hpp
+++ rdbms/types/operations.hpp
@@ -9,16 +9,16 @@ class operations{
     private:
         std::set<tuples> relation;
     public:
-        std::set<tuples> union();
+        std::set<tuples> union(std::set<tuples>R1 ,std::set<tuples> R2);
 
-        std::set<tuples> intersection();
+        std::set<tuples> intersection(std::set<tuples> R1 ,std::set<tuples> R2);
 
-        std::set<tuples> difference();
+        std::set<tuples> difference(std::set<tuples> R1 ,std::set<tuples> R2);
 
-        std::set<tuples> projection();
+        std::set<tuples> projection(std::set<tuples> R1);
 
-        std::set<tuples> selection();
+        std::set<tuples> selection(std::set<tuples> R1);
 
-        std::set<tuples> natural_join();        
+        std::set<tuples> natural_join(std::set<tuples> R1 ,std::set<tuples> R2);        
 
 };
