b0ca03eb3a6461e050c3bb6bdf7177efc6307f10
= <=>
2020-02-08 13:06:13 +0300

    Implimented operations difference

--- rdbms/types/operations.cpp
+++ rdbms/types/operations.cpp
@@ -56,7 +56,22 @@ table operations::intersection(table R1 ,table R2){
 }
 
 table operations::difference(table R1 ,table R2){
-    //TODO
+    if (tuple<T>::isEqual(R1.get_attributes() , R2.get_attributes()) == false){
+        throw "Invalid Table atteribute types can't perform Intersection of the two tables";
+    }
+
+    table::table difference (rSchema("runion",R1.get_attributes));
+
+    std::set<tuples> set1 = R1.get_rows();
+    std::set<tuples> set2 = R2.get_rows();
+    
+    std::set<tuples> set3; 
+    std::set_difference(set1.begin(),set1.end(),set2.begin(),set2.end(),std::back_inserter(set3));
+    for (const std::set<tuples> &i : set3) {
+            difference.add_row(i);
+        }
+
+
 }
 
 table operations::projection(table R1){
