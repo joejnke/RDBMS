18881bfd3f7a7794f567d039725ceeb0372b55e2
Kirubel K <joejnke@gmail.com>
2020-02-11 14:34:36 +0300

    Implement toString for cell

--- rdbms/util/cell.hpp
+++ rdbms/util/cell.hpp
@@ -9,4 +9,56 @@ using namespace std;
 
 typedef boost::variant<set<std::string>,set<double>,set<int>,double,std::string,int> cell;
 
+struct toString
+    : public boost::static_visitor<std::string>
+{
+public:
+
+    
+    std::string operator()(int & item ) const {
+        return std::to_string(item);
+    }
+
+    std::string operator()(double & item ) const {
+        return std::to_string(item);
+    }
+
+    std::string operator()(std::string & item ) const {
+        return item;
+    }
+
+    std::string operator()(set<int> & item ) const {
+        std::string result = "( ";
+        for (auto i : item) {
+            result += std::to_string(i) + " ";
+        }
+
+        result += ")";
+
+        return result;
+    }
+
+    std::string operator()(set<double> & item ) const {
+        std::string result = "( ";
+        for (auto i : item) {
+            result += std::to_string(i) + " ";
+        }
+
+        result += ")";
+
+        return result;
+    }
+
+    std::string operator()(set<std::string> & item ) const {
+        std::string result = "( ";
+        for (auto i : item) {
+            result += i + " ";
+        }
+        
+        result += ")";
+
+        return result;
+    }
+};
+
 #endif  /*CELL_H*/
