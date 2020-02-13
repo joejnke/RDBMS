244576c974f14732cba77d89fe5020274ec3ad82
Kirubel K <joejnke@gmail.com>
2020-02-13 22:29:26 +0300

    Fix table and rSchema related commands' implementation

--- rdbms/types/rdbms.cpp
+++ rdbms/types/rdbms.cpp
@@ -17,8 +17,10 @@ using namespace std;
 int main(){
     string datainput;
     vector<string> result; 
-    // map<std::string,table> tableMap;
-    // map<std::string,rSchema> rschemaMap;
+    map<std::string,table> tableMap;
+    map<std::string,rSchema> rschemaMap;
+    system("clear");
+
     cout<<"enter the comand separating it with a comma\n";
 
     do{
@@ -39,7 +41,7 @@ int main(){
                     if(result.size()<=3)
                         cout<<"error: no table name inserted\n";
                     else {
-                        // tableMap.insert(result[3],table (rschemaMap.get(result[2])));
+                        tableMap.emplace(result[3],table (rschemaMap.at(result[2])));
                         cout<<result[3]<<" table created\n";
                         result.clear();
                     }
@@ -53,7 +55,7 @@ int main(){
                 else if(result.size()<=3)
                     cout<<"error: yo need to insert the atribute names\n";
                 else {
-                    //  rschemaMap.insert(result[2],rSchema (result[2], tuples (result[3],result[4])));
+                     rschemaMap.emplace(result[2],rSchema (result[2], tuples (result[3],result[4])));
                     cout<<result[2]<<" rschema created\n";
                     result.clear();
                 }
@@ -71,6 +73,8 @@ int main(){
                 if(result.size()<=2)
                     cout<<"error: you have to insert the row data\n";
                 else {
+                   tableMap.at(result[1]).add_row (tuples (result[3], result[4])) ;   
+
                     cout<<"data inserted to: "<<result[1]<<endl;
                 }   
             }
@@ -86,6 +90,7 @@ int main(){
                     if(result.size()<=3)
                         cout<<"error: you have to insert the row to be removed\n";
                     else {
+                        tableMap.at(result[1]).remove_row(tuples (result[3], result[4]));
                         cout<<"data removed from: "<<result[2]<<endl;
                     }   
                 }
@@ -113,8 +118,10 @@ int main(){
                 cout<<"error: insert the first table name\n";
             else {
                 if(result.size()<=2)
-                    cout<<"error: you have to input the secont table name\n";
+                    cout<<"error: you have to input the second table name\n";
                 else {
+                    operations.runion(result[1],result[2]);
+
                     cout<<"display the union\n";
                 }
             }    
@@ -127,6 +134,8 @@ int main(){
                 if(result.size()<=2)
                     cout<<"error: you have to input the secont table name\n";
                 else {
+                    operations.intersection(result[1],result[2]);
+
                     cout<<"display the intersection\n";
                 }
             }    
@@ -139,6 +148,8 @@ int main(){
                 if(result.size()<=2)
                     cout<<"error: you have to input the secont table name\n";
                 else {
+                    operations.difference(result[1],result[2]);
+
                     cout<<"display the difference\n";
                 }
             }    
@@ -151,15 +162,18 @@ int main(){
                 if(result.size()<=2)
                     cout<<"error: you have to input attribute name\n";
                 else {
-                    cout<<"display the union\n";
+                    operations.projection(result[1]);
+                    cout<<"display the projection\n"; 
                 }
             }    
         }
 
         else if(result[0]=="select") {
             if(result.size()<=1)
-                cout<<"error: oyu have to input the table name\n";
+                cout<<"error: you have to input the table name\n";
             else {
+                operations.selection(result[1]);
+
                 cout<<"display the the selected data\n";
             }
         }
@@ -169,11 +183,22 @@ int main(){
                 cout<<"error: insert the first table name\n";
             else {
                 if(result.size()<=2)
-                    cout<<"error: you have to input the secont table name\n";
+                    cout<<"error: you have to input the second table name\n";
                 else {
+                    operations.natural_join(result[1],result[2]);
+
                     cout<<"display the natural join\n";
                 }
             }    
+        }
+
+        else if(result[0]=="cls") {
+            system("clear");
+        }
+
+        else {
+            if(result[0]!="exit")
+            cout<<"unknown command\n";
         }
         
         result.clear();
