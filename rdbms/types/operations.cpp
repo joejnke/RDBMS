b830fe70f77290df820b57e8c2d2a80747369650
Dibora Gebreyohannes <deborahaile3@email.com>
2020-02-13 09:51:53 +0300

    implement operations

--- rdbms/types/operations.cpp
+++ rdbms/types/operations.cpp
@@ -1,77 +1,67 @@
-#include "cell.hpp"
+#include "../util/cell.hpp"
 #include "operations.hpp"
 #include "rSchema.hpp"
 #include "table.hpp"
-#include "tuples.hpp"
+#include "../util/tuples.hpp"
 #include <iostream>
 #include <set>
 #include <algorithm>
 
 table operations::runion(table R1 ,table R2){
     //conditional statment will not work tuple comparing function must be implimented
-    if (tuple<T>::isEqual(R1.get_attributes() , R2.get_attributes()) == false){
+    if (!tuples::isEqual(R1.get_attributes() , R2.get_attributes())){
         throw "Invalid Table atteribute types can't Union the two tables";
     }
 
-    table::table runion (rSchema("runion",R1.get_attributes));
-    /*
-    std::set<tuples> set1 = R1.get_rows();
-    std::set<tuples> set2 = R2.get_rows();
-    
-    std::set<tuples> set3; 
-    std::set_union(set1.begin(),set1.end(),set2.begin(),set2.end(),std::back_inserter(set3));
-    for (const auto &i : set3) {
-            runion.add_row(i);
-        } 
-    
-    */
-
-    for (std::set<tuple<T>>::iterator it=R1.get_rows().begin(); it!=R1.get_rows().end(); ++it)
-        runion.add_row(*it);
-
-    for (std::set<tuple<T>>::iterator it2=R2.get_rows().begin(); it2!=R2.get_rows().end(); ++it2)
-        runion.add_row(*it2); 
+    table runion (rSchema("R1_R2_union",R1.get_attributes()));
+    for (std::set<tuples>::iterator itr1 = R1.get_rows().begin(); itr1 != R1.get_rows().end(); ++itr1) 
+    { 
+         runion.add_row(*itr1);
+    }
+    ////// adding the rows of the second table to the union
+        for (std::set<tuples>::iterator itr2 = R2.get_rows().begin(); itr2 != R2.get_rows().end(); ++itr2) 
+    {
+        runion.add_row(*itr2);
+    } 
 
     return runion;
 }
 
 table operations::intersection(table R1 ,table R2){
-    if (tuple<T>::isEqual(R1.get_attributes() , R2.get_attributes()) == false){
-        throw "Invalid Table atteribute types can't perform Intersection of the two tables";
+     if (!tuples::isEqual(R1.get_attributes() , R2.get_attributes())){
+        throw "Invalid Table atteribute types can't Union the two tables";
     }
 
-    table::table intersection (rSchema("runion",R1.get_attributes));
 
-    std::set<tuples> set1 = R1.get_rows();
-    std::set<tuples> set2 = R2.get_rows();
-    
-    std::set<tuples> set3; 
-    std::set_intersection(set1.begin(),set1.end(),set2.begin(),set2.end(),std::back_inserter(set3));
-    for (const std::set<tuples> &i : set3) {
-            intersection.add_row(i);
-        }
+    table::table intersect_table(rSchema("runion",R1.get_attributes));
 
+     for (std::set<tuples>::iterator itr1 = R1.get_rows().begin(); itr1 != R1.get_rows().end(); ++itr1) 
+    { 
+        for (std::set<tuples>::iterator itr2 = R2.get_rows().begin(); itr2 != R2.get_rows().end(); ++itr2) 
+    {
+         if(tuples::isEqual(*itr1,*itr2)){
+         intersect_table.add_row(*itr1);
+         }
+    }
 
+    }
 
+    return intersect_table;
 }
 
 table operations::difference(table R1 ,table R2){
-    if (tuple<T>::isEqual(R1.get_attributes() , R2.get_attributes()) == false){
-        throw "Invalid Table atteribute types can't perform Intersection of the two tables";
+   if (!tuples::isEqual(R1.get_attributes() , R2.get_attributes())){
+        throw "Invalid Table atteribute types can't Union the two tables";
     }
+    table::table difference_table(rSchema("runion",R1.get_attributes));
 
-    table::table difference (rSchema("runion",R1.get_attributes));
-
-    std::set<tuples> set1 = R1.get_rows();
-    std::set<tuples> set2 = R2.get_rows();
-    
-    std::set<tuples> set3; 
-    std::set_difference(set1.begin(),set1.end(),set2.begin(),set2.end(),std::back_inserter(set3));
-    for (const std::set<tuples> &i : set3) {
-            difference.add_row(i);
+     for (std::set<tuples>::iterator itr1 = R1.get_rows().begin(); itr1 != R1.get_rows().end(); ++itr1) 
+    { 
+        if(R2.get_rows().find(*itr1) == R2.get_rows().end()){
+            difference_table.add_row(*itr1);
         }
-
-
+    }
+    return difference_table;
 }
 
 table operations::projection(table R1){
