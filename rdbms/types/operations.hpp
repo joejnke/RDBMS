7f6c6f87870fae5922d9e60185ff0715a7f75e2a
DiboraHaile <53183728+DiboraHaile@users.noreply.github.com>
2020-02-22 23:18:42 +0300

    add operation documentation

--- rdbms/types/operations.hpp
+++ rdbms/types/operations.hpp
@@ -24,16 +24,41 @@
 class operations{
     public:
         static table runion (table R1 ,table R2);
-    
+        /**
+        *  @brief Do union operation on two tables.
+        *  @param the two tables the operation is done on.
+        *  @return table of their union.
+        */  
         static table intersection(table R1 ,table R2);
-    
+        /**
+        *  @brief Do intersection operation on two tables.
+        *  @param the two tables the operation is done on.
+        *  @return table of their intersection.
+        */ 
         static table difference(table R1 ,table R2);
-    
+        /**
+        *  @brief Do difference operation on two tables.
+        *  @param the two tables the operation is done on.
+        *  @return table of their difference.
+        */
         static table projection(table R1,string P);
-    
+        /**
+        *  @brief Project the data values of the selected attribute.
+        *  @param the table the operation is done on, and the attribute name for the projection.
+        *  @return table of the projected values.
+        */
         static table selection(table R1 , string A , string S);
-    
-        static table natural_join(table R1 ,table R2, string id1,string id2,string A1 ,string A2);       
+        /**
+        *  @brief project data from the table given the attribute name and the value
+        *  @param table the operation is done on, the attribute name and value
+        *  @return table of the projected data value
+        */
+        static table natural_join(table R1 ,table R2, string id1,string id2,string A1 ,string A2);    
+        /**
+        *  @brief project values from two tables with 
+        *  @param the two tables the data values are projected from, the attribute names used to compare the two tables and the attribute names the data is projected from.
+        *  @param table of the projected values.
+        */
 
 };
 
